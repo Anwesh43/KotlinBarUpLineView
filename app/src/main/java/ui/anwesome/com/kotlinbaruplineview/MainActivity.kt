a0d2ce4fe0f7a0f20e6486c6be8b2296b867af0c
Anwesh43 <anweshthecool0@gmail.com>
2018-04-23 09:45:53 +0530

    adding View in activity

--- app/src/main/java/ui/anwesome/com/kotlinbaruplineview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbaruplineview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinbaruplineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.baruplineview.BarUpLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BarUpLineView.create(this)
     }
 }
